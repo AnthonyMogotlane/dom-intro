818e81bbc3d05d7474965f5ba07d01f74b22ebdc
Anthony <almogotlane@gmail.com>
2022-04-26 16:11:57 +0200

    update calculate-bill.js

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -15,18 +15,20 @@ document.querySelector(".calculateBtn").addEventListener("click", () => {
     // a variable for the total phone bill.
     var billTotal = 0;
     //loop over all the bill items
-    for (var i=0;i<billItems.length;i++){
+    for (var i = 0; i < billItems.length; i++) {
         var billItem = billItems[i].trim();
-        if (billItem === "call"){
+        if (billItem === "call") {
             billTotal += 2.75;
         }
-        else if (billItem === "sms"){
+        else if (billItem === "sms") {
             billTotal += 0.75;
         }
     }
-    
     //round to two decimals
     var roundedBillTotal = billTotal.toFixed(2);
     billTotalElement.innerHTML = roundedBillTotal;
+
+    if (roundedBillTotal >= 30) billTotalElement.style.color = "orange";
+    if (roundedBillTotal >= 50) billTotalElement.style.color = "red";
 })
 //link the function to a click event on the calculate button
