d7fbabc120e16fde9435205409b959a3191d5d9f
Anthony Mogotlane <almogotlane@gmail.com>
2022-05-19 06:15:26 +0200

    Modify text-bill.js
    change the logic to use templates and reconnect the logic to business logic

--- js/text-bill.js
+++ js/text-bill.js
@@ -1,47 +1,49 @@
-//===========================================================================
-// get a reference to the textbox where the bill type is to be entered
+//reference to the textbox where the bill type is to be entered
 const billTypeText = document.querySelector(".billTypeText");
-//get a reference to the add button
+//reference to the add button
 const addToBillBtn = document.querySelector(".addToBillBtn");
-//reference to the call bill
-const callTotalOne = document.querySelector(".callTotalOne");
-//reference to the sms bill
-const smsTotalOne = document.querySelector(".smsTotalOne");
-//create a variable that will keep track of the total bill
-const totalOne = document.querySelector(".totalOne")
 
 //instance of calculateBill
 let textBillCosts = calculateBill();
 
 const textBillTotal = () => {
-    if(!["call", "sms"].includes(billTypeText.value.toLowerCase())) alert("invalid input! enter 'call' or 'sms'");
-    let totalCosts = 0;
-    if(billTypeText.value.toLowerCase() === "call") {
-        textBillCosts.setBill(billTypeText.value);
-        //incrementing the call costs with call made
-        callTotalOne.textContent = (parseFloat(callTotalOne.textContent) + textBillCosts.getCallCost()).toFixed(2);
-        //incrementing the total costs with call made
-        totalCosts += (parseFloat(totalOne.textContent) + textBillCosts.getCallCost())
-        totalOne.textContent = totalCosts.toFixed(2);
-    }
-    if(billTypeText.value.toLowerCase() === "sms") {
-         //incrementing the call costs with call made
-         smsTotalOne.textContent = (parseFloat(smsTotalOne.textContent) + textBillCosts.getSmsCost()).toFixed(2);
-         //incrementing the total costs with Sms made
-         totalCosts += (parseFloat(totalOne.textContent) + textBillCosts.getSmsCost())
-         totalOne.textContent = totalCosts.toFixed(2);
-    }
+    // //if a relevent limit is reached set the className
+    // let className = "";
+    // (totalOne.textContent - "" >= 50) ? className = "danger"
+    // :(totalOne.textContent - "" >= 30) ? className = "warning"
+    // :(totalOne.textContent - "" < 30) ? className = "dark"
+    // :"";
 
-    //if a relevent limit is reached set the className
-    let className = "";
-    (totalOne.textContent - "" >= 50) ? className = "danger"
-    :(totalOne.textContent - "" >= 30) ? className = "warning"
-    :(totalOne.textContent - "" < 30) ? className = "dark"
-    :"";
+    // //changing the total color when relevent limit is reached
+    // colorIndicator(className, totalOne);
 
-    //changing the total color when relevent limit is reached
-    colorIndicator(className, totalOne);
+    if (textBillCosts.setBill(billTypeText.value) === "call") {
+        textBillCosts.setCallCost();
+    } else if (textBillCosts.setBill(billTypeText.value) === "sms") {
+        textBillCosts.setSmsCost();
+    }
+    handlebarsTemplate(textBillCosts.getCallCost(), textBillCosts.getSmsCost(), textBillCosts.getTotalBill())
 }
-
 addToBillBtn.addEventListener("click", textBillTotal);
+
+function handlebarsTemplate(call, sms, total) {
+
+    //reference to the template
+    let template = document.querySelector(".table-content").innerHTML;
+    //compile the template
+    let templateFunction = Handlebars.compile(template);
+
+    var billTotals = {
+        callTotalBillOne: call.toFixed(2),
+        smsTotalBillOne: sms.toFixed(2),
+        totalBillOne: total.toFixed(2)
+    }
+
+    document.querySelector(".bill-table").innerHTML = templateFunction(billTotals);
+}
+handlebarsTemplate(0, 0, 0)
+
+
+
+
 
